05dec1333d9baeb876d08fab9d9fd3316c9de3b5
James jr <jameslu91223@gmail.com>
2022-05-27 02:09:03 +0800

    Day 21

---
+++ Day 21 191. Number of 1 Bits.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int ans = 0;
+        for (int i = 0; i < 32; ++i)
+        {
+            if (n % 2 == 1)
+                ans++;
+            n /= 2;
+        }
+        return ans;
+    }
+};
